3b6697d6f5a1da5b43c65e1d4c47848d8da59a13
Lala Agadjanyan <gevorgyanlala@mail.ru>
2023-05-30 11:38:34 +0300

    commit 2

--- glossary_of_names.php
+++ glossary_of_names.php
@@ -5,7 +5,7 @@ class IexMergeProps {
     public static $glossaryMergeProp = [
         "name_duplicate_property" => "name_main_property",
     ];
-
+//comment 1
     //$ArrIBlocId массив с ID каталогов: торгового каталога и торговых предложений
     public static $arIBlocId = [
         "CATALOG_IBLOCK_ID",
